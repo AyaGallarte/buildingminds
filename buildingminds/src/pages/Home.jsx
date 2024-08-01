118431611c0db7a05c4f421f7200b5056c9b054b
aya.gallarte <aya.gallarte@gmail.com>
2024-08-02 07:30:53 +0800

    add home page

---
+++ buildingminds/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { Row, Col, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../index.css';
+
+export default function Home() {
+    return (
+           <div className="page-container">
+            <div className="content-wrapper">
+                <h1>Yes, it's working</h1>
+            </div>
+        </div>
+    );
+}
+
